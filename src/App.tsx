2a49d5698ec26168806f7d3b1db39c59295ce006
Hernandez <lu97is@gmail.com>
2021-04-19 19:36:20 -0500

    using component

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Buttom from '@bit/rober97to.geeknight.button';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Buttom />
+    </div>
+  );
+}
+
+export default App;
